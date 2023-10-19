564d2de5615a5b776b42a9b93807fdb9a4bb273a
Sebastiano Vigna <sebastiano.vigna@gmail.com>
2023-10-19 15:57:24 +0200

    Simplified, added test

--- src/lib.rs
+++ src/lib.rs
@@ -21,12 +21,65 @@ pub use self::traits::Item;
 pub use self::traits::LendingIterator;
 pub use self::traits::LendingIteratorItem;
 
+/// A macro to iterate easily over an [`IntoLendingIterator`].
+/// The syntax makes it possible to write loops such as
+/// ```ignore
+/// for_lend!{x in into_iter =>
+/// 	println!("{}", x);
+/// }
+/// ```
 #[macro_export]
 macro_rules! for_lend {
     ($var:ident in $iter:expr => $($tt:tt)*) => {
-        let mut iter = $iter.into_iter();
+        let mut iter = $iter.into_lend_iter();
         while let Some($var) = iter.next() {
             $($tt)*
         }
     }
+}
+
+#[test]
+fn test_macro() {
+    // Mock impl
+    struct Mock();
+
+    impl Mock {
+        pub fn iter(&self) -> MockLendingIterator {
+            MockLendingIterator {}
+        }
+    }
+
+    struct MockLendingIterator {}
+
+    impl<'a> LendingIteratorItem<'a> for MockLendingIterator {
+        type T = &'a str;
+    }
+
+    impl LendingIterator for MockLendingIterator {
+        fn next(&mut self) -> Option<Item<'_, Self>> {
+            None
+        }
+    }
+
+    fn test_mock_lend_iter(m: Mock) {
+        for_lend! {x in m.iter() =>
+            println!("{}", x);
+        };
+    }
+
+    impl IntoLendingIterator for &Mock {
+        type IntoIter = MockLendingIterator;
+        fn into_lend_iter(self) -> Self::IntoIter {
+            self.iter()
+        }
+    }
+
+    fn test_mock_into_lend_iter(m: Mock) {
+        for_lend! {x in &m =>
+            println!("{}", x);
+        };
+    }
+
+    test_mock_lend_iter(Mock {});
+    test_mock_into_lend_iter(Mock {});
 }
--- src/traits/into.rs
+++ src/traits/into.rs
@@ -4,13 +4,12 @@
  * SPDX-License-Identifier: Apache-2.0 OR LGPL-2.1-or-later
  */
 
-use crate::{Item, LendingIterator, LendingIteratorItem};
+use crate::LendingIterator;
 
 pub trait IntoLendingIterator {
     /// Which kind of iterator are we turning this into?
     type IntoIter: LendingIterator;
 
-    /// Creates an iterator from a value.
     fn into_lend_iter(self) -> Self::IntoIter;
 }
 
