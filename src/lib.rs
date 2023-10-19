c5e7b8ca6220a48e09f799b50983a51b1109f9a7
Sebastiano Vigna <sebastiano.vigna@gmail.com>
2023-10-19 13:52:13 +0200

    No more item

--- src/lib.rs
+++ src/lib.rs
@@ -23,8 +23,8 @@ pub use self::traits::LendingIteratorItem;
 
 #[macro_export]
 macro_rules! for_lend {
-    ($var:ident in $iter:expr => {$($tt:tt)*}) => {
-        let mut iter = $iter.into_lend_iter();
+    ($var:ident in $iter:expr => $($tt:tt)*) => {
+        let mut iter = $iter.into_iter();
         while let Some($var) = iter.next() {
             $($tt)*
         }
--- src/traits/into.rs
+++ src/traits/into.rs
@@ -4,13 +4,20 @@
  * SPDX-License-Identifier: Apache-2.0 OR LGPL-2.1-or-later
  */
 
-use crate::{LendingIterator, LendingIteratorItem};
+use crate::{Item, LendingIterator, LendingIteratorItem};
 
 pub trait IntoLendingIterator {
-    type Item<'a>;
     /// Which kind of iterator are we turning this into?
-    type IntoIter: LendingIterator + for<'a> LendingIteratorItem<'a, T = Self::Item<'a>>;
+    type IntoIter: LendingIterator + for<'a> LendingIteratorItem<'a>;
 
     /// Creates an iterator from a value.
     fn into_lend_iter(self) -> Self::IntoIter;
+}
+
+impl<I: LendingIterator> IntoLendingIterator for I {
+    type IntoIter = Self;
+
+    fn into_lend_iter(self) -> Self::IntoIter {
+        self
+    }
 }
